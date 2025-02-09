7dd681bdda8c45f3f953ed1564ae8b3d251cf70e
vincent <vincent.z.chao@gmail.com>
2025-02-10 01:38:32 +0800

    feat: Add initial project structure and dependencies

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # spring-boot-scaffold
+
+## 项目介绍
+
+Spring Boot 项目脚手架
+
+## 特性列表
+
+1. 通用 `.gitignore` 文件。
+2. 通用 `pom.xml` 文件。
+
+## TODO
+
+1. 代码格式化相关。
+2. Maven 打包相关。
---
+++ webmvc-scafflod/src/main/java/com/binchaos/Application.java
@@ -0,0 +1,14 @@
+package com.binchaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vincent
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
